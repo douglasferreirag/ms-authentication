f726366667b4c5c122de27c6c0aeac78a4b1596f
douglasferreirag <douglas.ferreirag@gmail.com>
2022-05-31 21:43:34 -0300

    CRUD sem banco de dados

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
 import express, {Request, Response, NextFunction} from 'express';
 
+import usersRoute from './routes/users.route';
+
 const app = express();
+
+//Configurações da aplicação.
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(usersRoute);
+
+
+
+// Configurações de rotas.
 
 app.get('/status', (req: Request,res: Response,next: NextFunction) =>{
 
     res.status(200).send({foo : 'Sucesso total!! Vocês são demais!'})
 
 });
+
+//Inicialização do servidor.
 
 app.listen(3000, () => {
 
